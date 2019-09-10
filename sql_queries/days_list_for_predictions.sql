936503ab65d06ebb6ab7658de088935c3f08d921
Alexey Yakunin <alexey_yakunin@epam.com>
2019-09-10 15:26:38 +0300

    is billable script for making predictions

--- sql_queries/days_list_for_predictions.sql
+++ sql_queries/days_list_for_predictions.sql
@@ -1,4 +1,13 @@
-select distinct(date)
-from wpm_anlt.epm_wpm_ds_staff_{0}_probability_predictions
-where date::date >= '{1}'::date and date::date <= '{2}'::date
+select date
+from (
+	select date, count(scope)
+	from (
+		select distinct date, scope
+		from wpm_anlt.epm_wpm_ds_staff_rev_in_staf_{0}
+        where date::date >= '{1}'::date
+        and date::date <= '{2}'::date
+		) d
+	group by date
+	) c
+where count = 3
 order by date
